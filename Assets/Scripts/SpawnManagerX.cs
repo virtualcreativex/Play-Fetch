f21d41d1ae8ee345ccb92eb50d37941fe941c5b7
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-10-30 21:07:35 -0500

    Fix SpawnManager Balls ramdom spawning.

--- Assets/Scripts/SpawnManagerX.cs
+++ Assets/Scripts/SpawnManagerX.cs
@@ -25,8 +25,10 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        
+        int index = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
     }
 
     private float counter = 0f;
